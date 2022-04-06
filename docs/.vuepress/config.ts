602d583a1f6e5cb169d86e712dae3ef2cdd04b76
sunzeyu <1549522434@qq.com>
2022-04-06 13:00:17 +0800

    feat 初始化blog

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,159 @@
+/**
+ * 提示：如您想使用JS版本的配置文件可参考：https://github.com/xugaoyi/vuepress-theme-vdoing/tree/a2f03e993dd2f2a3afdc57cf72adfc6f1b6b0c32/docs/.vuepress
+ */
+import { resolve } from 'path'
+import { defineConfig4CustomTheme, UserPlugins } from 'vuepress/config'
+import { VdoingThemeConfig } from 'vuepress-theme-vdoing/types'
+// @ts-ignore
+import dayjs from 'dayjs'
+import htmlModules from './config/htmlModules' // 自定义插入的html块
+
+export default defineConfig4CustomTheme<VdoingThemeConfig>({
+  theme: resolve(__dirname, '../../vdoing'), // 使用本地主题
+
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: "OneGs",
+      description: 'web前端技术博客,专注web前端学习与总结。',
+    }
+  },
+
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '前端',
+        link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+        items: [
+          // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
+          {
+            text: '前端文章',
+            items: [
+              { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
+            ],
+          },
+          {
+            text: '学习笔记',
+            items: [
+              { text: '《JavaScript教程》', link: '/note/javascript/' },
+              { text: '《JavaScript高级程序设计》', link: '/note/js/' },
+              { text: '《ES6 教程》', link: '/note/es6/' },
+              { text: '《Vue》', link: '/note/vue/' },
+              { text: '《React》', link: '/note/react/' },
+              {
+                text: '《TypeScript 从零实现 axios》',
+                link: '/note/typescript-axios/',
+              },
+              {
+                text: '《Git》',
+                link: '/note/git/',
+              },
+              {
+                text: 'TypeScript',
+                link: '/pages/51afd6/',
+              },
+              {
+                text: 'JS设计模式总结',
+                link: '/pages/4643cd/',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        text: '技术',
+        link: '/technology/',
+        items: [
+          { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
+          { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
+          { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
+          { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+        ],
+      },
+      {
+        text: '更多',
+        link: '/more/',
+        items: [
+          { text: '学习', link: '/pages/f2a556/' },
+          { text: '面试', link: '/pages/aea6571b7a8bae86/' },
+          { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
+          { text: '实用技巧', link: '/pages/baaa02/' },
+          { text: '友情链接', link: '/friends/' },
+        ],
+      },
+    ],
+    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    logo: '/img/logo.png', // 导航栏logo
+    searchMaxSuggestions: 10, // 搜索结果显示最大数
+    lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
+    docsDir: 'docs', // 编辑的文件夹
+    editLinks: true, // 启用编辑
+    editLinkText: '编辑',
+    archive: false, // 是否打开归档功能，默认true
+    bodyBgImg: [
+      'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175828.jpeg',
+      'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175845.jpeg',
+      'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175846.jpeg'
+    ], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时每隔15秒换一张。
+    bodyBgImgOpacity: 0.5, // body背景图透明度，选值 0.1~ 1.0, 默认0.5
+    titleBadge: false, // 文章标题前的图标是否显示，默认true
+    contentBgStyle: 5, // 文章内容块的背景风格，默认无. 1 方格 | 2 横线 | 3 竖线 | 4 左斜线 | 5 右斜线 | 6 点状
+
+    // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | <自定义>    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+    sidebar: 'structuring',
+
+    // 文章默认的作者信息，(可在md文件中单独配置此信息) string | {name: string, link?: string}
+    author: {
+      name: 'onegs', // 必需
+    },
+
+    // 博主信息 (显示在首页侧边栏)
+    blogger: {
+      avatar: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
+      name: 'OneGs',
+      slogan: '...',
+    },
+  },
+
+  // 插件配置
+  plugins: <UserPlugins>[
+    'fulltext-search',
+    [
+      'one-click-copy', // 代码块复制按钮
+      {
+        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+        copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+        duration: 1000, // prompt message display time.
+        showInMobile: false, // whether to display on the mobile side, default: false.
+      },
+    ],
+    [
+      'vuepress-plugin-zooming', // 放大图片
+      {
+        selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
+        options: {
+          bgColor: 'rgba(0,0,0,0.6)',
+        },
+      },
+    ],
+    [
+      '@vuepress/last-updated', // "上次更新"时间格式
+      {
+        transformer: (timestamp, lang) => {
+          return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+        },
+      },
+    ],
+  ],
+
+  markdown: {
+    lineNumbers: true,
+    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+  },
+
+  // 监听文件变化并重新构建
+  extraWatchFiles: [
+    '.vuepress/config.ts',
+  ]
+})
---
+++ docs/index.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroText: OneGs
+tagline: Web前端技术博客，积跬步以至千里，致敬每个爱学习的你。
+
+features:
+  - title: 前端
+    details: JavaScript、ES6、Vue框架等前端技术
+    link: /web/ # 可选
+    imgUrl: /img/web.png # 可选
+  - title: 技术
+    details: 技术文档、教程、技巧、总结等文章
+    link: /technology/
+    imgUrl: /img/other.png
