8a91837aa9f896c69f154698d6fe880a9c171a7f
Sunzeyu <1549522434@qq.com>
2022-09-14 10:29:53 +0800

    feat: 删除sudo

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ git pull origin master
 
 yarn build
 
-sudo rm -rf /var/www/175.178.221.4/html/*
+rm -rf /var/www/175.178.221.4/html/*
 
-sduo mv docs/.vuepress/dist/* /var/www/175.178.221.4/html
+mv docs/.vuepress/dist/* /var/www/175.178.221.4/html
 
 echo "deploy complete success"
